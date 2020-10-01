cbfe4946726009bf65e8415843b09c21a7762528
dmsapereira <david.pinguim2@hotmail.com>
2020-10-01 18:44:21 +0100

    Done. -l for lists, -L for everyone's from Lisbon

--- server.ts
+++ server.ts
@@ -2,6 +2,9 @@ const pgPromise = require('pg-promise');
 const R         = require('ramda');
 const request   = require('request-promise');
 
+const LIST_FLAG = "-l";
+const LISBON_FLAG = "-L";
+
 // Limit the amount of debugging of SQL expressions
 const trimLogsSize : number = 200;
 
@@ -39,36 +42,77 @@ const pgpDefaultConfig = {
   }
 };
 
-/*
-interface GithubUsers
-  { id : number
-  };
-  */
-
 interface GithubUser{
   id: number,
   login: Text,
   name: Text,
   company: Text,
+  location: Text,
+  email: Text,
+  followers: number,
+  following: number
+}
+
+interface LocationStatistic{
+  population: number,
   location: Text
 }
 
-function printUserInfo(data : GithubUser){
-  console.info(`ID: ${data.id}; Login: ${data.login}; Name: ${data.name}; Company: ${data.company}; Location: ${data.location}`);
-}
 
 const pgp = pgPromise(pgpDefaultConfig);
 const db = pgp(options);
 
+//Printing functions
+function printUserInfo(data : GithubUser, lisbonMaskFlag : boolean){
+  console.info(`
+  ID: ${data.id}; 
+  Login: ${data.login}; 
+  Name: ${data.name}; 
+  Company: ${data.company}; 
+  Location: ${lisbonMaskFlag ? data.location : "Lisbon"};
+  E-Mail: ${data.email};
+  Followers: ${data.followers};
+  Following: ${data.following}`);
+}
+
+function printLocationStats(data : LocationStatistic){
+  console.info(`Location: ${data.location}; Population: ${data.population}`);
+}
+
+//Database(Postgres) Functions
+
 async function createUsersTable(){
-  await db.none('CREATE TABLE github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT, location TEXT)')
+  await db.none('CREATE TABLE github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT, location TEXT, email TEXT, followers INT, following INT)')
     .catch(() => console.log("Table already exists"))
 }
 
 
 async function addUser(data : GithubUser){
-  await db.one('INSERT INTO github_users VALUES ($[id], $[login], $[name], $[company], $[location]) RETURNING id', data);
+  await db.one('INSERT INTO github_users VALUES ($[id], $[login], $[name], $[company], $[location], $[email], $[followers], $[following]) RETURNING id', data);
 }
+
+async function duplicateUser(login : String){
+  try{
+    let row = await db.one(`SELECT 1 FROM github_users WHERE login = ${login}`);
+  }catch(error){
+    return false;
+  }
+
+  return true;
+}
+
+async function listUsers(lisbonMaskFlag : boolean){
+  console.info("USERS")
+  await db.each('SELECT * FROM github_users',null, (data : GithubUser) => printUserInfo(data, lisbonMaskFlag));
+}
+
+async function listLocationStatistics(){
+  console.info("LOCATION STATISTICS")
+  await db.each('SELECT COUNT(*) AS population, location FROM github_users GROUP BY location',null, printLocationStats);
+}
+
+//Github Functions
+
 async function getGithubUser(login : String){
   return await request({
     uri: `https://api.github.com/users/${login}`,
@@ -79,35 +123,30 @@ async function getGithubUser(login : String){
   });
 }
 
-async function duplicateUser(login : String){
-  try{
-    let row = await db.one(`SELECT 1 FROM github_users WHERE login = ${login}`);
-  }catch(error){
-    return true;
-  }
-
-  return false;
-}
-
-async function listUsers(){
-  await db.each('SELECT * FROM github_users',null, (data : GithubUser) => printUserInfo(data));
-}
-
-async function dbOperation(login : String, listFlag : boolean){
+//"Main"
+async function dbOperation(login : String, listFlag : boolean, lisbonMaskFlag : boolean){
     await createUsersTable();
     if(await duplicateUser(login)){
-      let userData = await getGithubUser(login);
-      await addUser(userData);
+      let userData = undefined;
+      try{
+        userData = await getGithubUser(login);
+      }catch(error){
+        console.error("User does not exist on Github"); 
+        process.exit(0);
+      }
+        await addUser(userData);
     }else{
       console.error("User already exists in the database");
     }
 
     if(listFlag){
-      await listUsers();
+      await listUsers(lisbonMaskFlag);
     }
+
+    await listLocationStatistics();
 
     process.exit(0);
 }
 
 
-dbOperation(process.argv[2], process.argv[3] == "-l");
+dbOperation(process.argv[2], process.argv.includes(LIST_FLAG), process.argv.includes(LISBON_FLAG));
