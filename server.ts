f2830b972d99b5fc82852fdc97392d7a61dd99e2
dmsapereira <david.pinguim2@hotmail.com>
2020-10-01 17:55:14 +0100

    Rebased the structure of the script to use async functions. The then() chains were becoming unreadable

--- server.ts
+++ server.ts
@@ -39,25 +39,75 @@ const pgpDefaultConfig = {
   }
 };
 
+/*
 interface GithubUsers
   { id : number
   };
+  */
+
+interface GithubUser{
+  id: number,
+  login: Text,
+  name: Text,
+  company: Text,
+  location: Text
+}
+
+function printUserInfo(data : GithubUser){
+  console.info(`ID: ${data.id}; Login: ${data.login}; Name: ${data.name}; Company: ${data.company}; Location: ${data.location}`);
+}
 
 const pgp = pgPromise(pgpDefaultConfig);
 const db = pgp(options);
 
-let githubUser = process.argv[2];
+async function createUsersTable(){
+  await db.none('CREATE TABLE github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT, location TEXT)')
+    .catch(() => console.log("Table already exists"))
+}
 
-db.none('CREATE TABLE github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT)')
-.catch((error) => console.log(error))
-.then(() => request({
-  uri: `https://api.github.com/users/${githubUser}`,
-  headers: {
-        'User-Agent': 'Request-Promise'
+
+async function addUser(data : GithubUser){
+  await db.one('INSERT INTO github_users VALUES ($[id], $[login], $[name], $[company], $[location]) RETURNING id', data);
+}
+async function getGithubUser(login : String){
+  return await request({
+    uri: `https://api.github.com/users/${login}`,
+    headers: {
+      'User-Agent': 'Request-Promise'
     },
-  json: true
-  })
-).then((data: GithubUsers) => db.one(
-  'INSERT INTO github_users (login, name, company) VALUES ($[login], $[name], $[company]) RETURNING id', data)
-).then(({id}) => console.log(id))
-.then(() => process.exit(0));
+    json: true
+  });
+}
+
+async function duplicateUser(login : String){
+  try{
+    let row = await db.one(`SELECT 1 FROM github_users WHERE login = ${login}`);
+  }catch(error){
+    return true;
+  }
+
+  return false;
+}
+
+async function listUsers(){
+  await db.each('SELECT * FROM github_users',null, (data : GithubUser) => printUserInfo(data));
+}
+
+async function dbOperation(login : String, listFlag : boolean){
+    await createUsersTable();
+    if(await duplicateUser(login)){
+      let userData = await getGithubUser(login);
+      await addUser(userData);
+    }else{
+      console.error("User already exists in the database");
+    }
+
+    if(listFlag){
+      await listUsers();
+    }
+
+    process.exit(0);
+}
+
+
+dbOperation(process.argv[2], process.argv[3] == "-l");
